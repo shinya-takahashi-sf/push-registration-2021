fb6a07a14cacc8d52bdd740f20549e6dd67c3a26
Shinya Takahashi <shinya.takahashi@salesforce.com>
2021-10-21 15:43:27 +0900

    -

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
 return array(
     'appsignature' => 'none', 
-	'clientid' => getenv('APP_CENTER_API_INTEGRATION_CLIENTID'),
-	'clientsecret' => getenv('APP_CENTER_API_INTEGRATION_CLIENT_SECRET'),
+	// 'clientid' => getenv('APP_CENTER_API_INTEGRATION_CLIENTID'),
+	// 'clientsecret' => getenv('APP_CENTER_API_INTEGRATION_CLIENT_SECRET'),
+	'clientid' => 'seb3wfnqijryn18o8984stxd',
+	'clientsecret' => '5Iu9pGRty9uKUhpZ70zDn7GA',
 	'defaultwsdl' => 'https://webservice.exacttarget.com/etframework.wsdl',
     'xmlloc' => './ExactTargetWSDL.xml',
 );
